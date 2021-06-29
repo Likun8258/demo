6782e3fc036b646d8d6072a840899e961d24dd96
likun <KunLi@huaxing.com>
2021-06-29 15:23:36 +0800

    first commit

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "demo")
+public class DemoController {
+
+    @GetMapping(value = "test")
+    public String test(){
+        System.out.println("hahahhahha");
+        return "哈哈哈哈😄";
+    }
+
+}
